e12f4eee22f2ef450b5cc4883c3c7c0b40629425
Shurpaev Gadji-Ata <shurpaevgadjiata@gmail.com>
2025-06-05 00:43:07 +0300

    Add files via upload

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+-- Создаем таблицы с правильными именами полей
 CREATE TABLE applications (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fio VARCHAR(150) NOT NULL,
@@ -7,23 +8,27 @@ CREATE TABLE applications (
     gender ENUM('male', 'female') NOT NULL,
     bio TEXT NOT NULL,
     agreement BOOLEAN NOT NULL DEFAULT 0,
+    login VARCHAR(50) UNIQUE,
+    password_hash VARCHAR(255),
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE languages (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE application_languages (
     app_id INT UNSIGNED NOT NULL,
     lang_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (app_id, lang_id),
     FOREIGN KEY (app_id) REFERENCES applications(id) ON DELETE CASCADE,
     FOREIGN KEY (lang_id) REFERENCES languages(id) ON DELETE CASCADE
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO languages (name) VALUES ('Pascal'), ('C'), ('C++'), ('JavaScript'), ('PHP'), ('Python'), ('Java'), ('Haskel'), ('Clojure'), ('Prolog'), ('Scala'), ('Go');
-
-ALTER TABLE applications ADD COLUMN login VARCHAR(50) UNIQUE;
-ALTER TABLE applications ADD COLUMN password_hash VARCHAR(255);
+-- Заполняем языки
+INSERT INTO languages (name) VALUES 
+('Pascal'), ('C'), ('C++'), ('JavaScript'), ('PHP'),
+('Python'), ('Java'), ('Haskell'), ('Clojure'), ('Prolog'),
+('Scala'), ('Go');
